da97c1fce0c45e6579eb2df7f23be1124e6a33fe
chengcli <chengcli@umich.edu>
2023-05-19 10:00:55 +0100

    fix cmake

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -2,6 +2,17 @@ include(FetchContent)
 
 set(FETCHCONTENT_QUIET FALSE)
 
+# Execute python3-config to get the include flags
+execute_process(
+    COMMAND python3-config --includes
+    OUTPUT_VARIABLE PYTHON_INCLUDE_FLAGS
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+# 3.2.2 Extract the include path from the flags
+#string(REGEX MATCH "-I([^\\\"]+)" PYTHON_INCLUDE_PATH "${PYTHON_INCLUDE_FLAGS}")
+# 3.2.3 Add the Python include path to include_directories
+#include_directories(${PYTHON_INCLUDE_PATH})
+
 FetchContent_Declare(
     pybind11
     URL https://github.com/pybind/pybind11/archive/v2.10.0.tar.gz
