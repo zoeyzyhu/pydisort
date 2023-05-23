c9433fdf101744647ac6bc921ae28386a2186de5
chengcli <chengcli@umich.edu>
2023-05-23 09:03:18 -0400

    add cmake hook

--- cmake/tomlpp.cmake
+++ cmake/tomlpp.cmake
@@ -3,18 +3,17 @@ include(FetchContent)
 set(FETCHCONTENT_QUIET FALSE)
 
 FetchContent_Declare(
-    tomlpp
-    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-    URL https://github.com/marzer/tomlplusplus/archive/refs/tags/v3.3.0.tar.gz
-    )
+  tomlpp
+  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+  URL https://github.com/marzer/tomlplusplus/archive/refs/tags/v3.3.0.tar.gz)
 
 FetchContent_GetProperties(tomlpp)
 
 if(NOT tomlpp_POPULATED)
-    FetchContent_Populate(tomlpp)
-    add_subdirectory(${tomlpp_SOURCE_DIR} ${tomlpp_BINARY_DIR})
+  FetchContent_Populate(tomlpp)
+  add_subdirectory(${tomlpp_SOURCE_DIR} ${tomlpp_BINARY_DIR})
 endif()
 
-set(TOMLPP_INCLUDE_DIR ${tomlpp_SOURCE_DIR}/include
-    CACHE PATH 
-    "include directory of toml++/toml.h")
+set(TOMLPP_INCLUDE_DIR
+    ${tomlpp_SOURCE_DIR}/include
+    CACHE PATH "include directory of toml++/toml.h")
