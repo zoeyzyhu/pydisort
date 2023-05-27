ad8f8d317e5a42fa4ed3750d536c070de700d21e
zoey.zyhu <zoey.zyhu@gmail.com>
2023-05-26 20:00:01 -0400

    Ignore runtime/references

--- tools/check_style.sh
+++ tools/check_style.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-filters=-legal/copyright,-build/include_subdir,runtime/references
+filters=-legal/copyright,-build/include_subdir,-runtime/references
 
 # cppdisort
 cpplint --filter=${filters} --recursive src/cppdisort
