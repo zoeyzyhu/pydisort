961740feacd542c26579f98f6872e8df19ca110e
zoey.zyhu <zoey.zyhu@gmail.com>
2023-05-26 20:09:40 -0400

    Reorganize, add pylint

--- tools/check_style.sh
+++ tools/check_style.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# 1. Style checks for C++ ====================================================
+
 filters=-legal/copyright,-build/include_subdir,-runtime/references
 
 # cppdisort
@@ -7,3 +9,10 @@ cpplint --filter=${filters} --recursive src/cppdisort
 
 # pybind_cppdisort
 cpplint --filter=${filters} --recursive python/*.cpp
+
+# test_cppdisort
+cpplint --filter=${filters} --recursive tests/cppdisort
+
+# 2. Style checks for Python =================================================
+
+pylint --recursive=y tests/python
