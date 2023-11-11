2d9729eb3f893d523211ee43dd7e8c2f38ffc427
mac/cli <chengcli@umich.edu>
2023-11-10 22:08:06 -0500

    wip

--- tests/python/test_isotropic_scattering.py
+++ tests/python/test_isotropic_scattering.py
@@ -1,7 +1,6 @@
 #! python3
 """ Test isotropic scattering with pydisort."""
 # pylint: disable = no-name-in-module, invalid-name, import-error
-import os
 import unittest
 from numpy import array, pi
 from numpy.testing import assert_allclose
