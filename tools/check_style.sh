394583bdba4f85e21b4f9ccba3c88391811b58aa
mac/cli <chengcli@umich.edu>
2024-04-02 10:51:42 -0400

    wip

--- tools/check_style.sh
+++ tools/check_style.sh
@@ -5,14 +5,14 @@
 filters=-legal/copyright,-build/include_subdir,-build/include_order,-runtime/references
 
 # cppdisort
-cpplint --filter=${filters} --recursive src/cppdisort
+#cpplint --filter=${filters} --recursive src/cppdisort
 
 # pybind_cppdisort
-cpplint --filter=${filters} --recursive python/*.cpp
+#cpplint --filter=${filters} --recursive python/*.cpp
 
 # test_cppdisort
-cpplint --filter=${filters} --recursive tests/cppdisort
+#cpplint --filter=${filters} --recursive tests/cppdisort
 
 # Style checks for Python =================================================
 
-pylint --recursive=y tests/python
+#pylint --recursive=y tests/python
