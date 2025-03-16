da3ef3b51fe032abd418cd449ec91514a9a5760e
mac/cli <chengcli@umich.edu>
2025-03-16 10:17:17 -0400

    wip

--- tests/test_attenuation.py
+++ tests/test_attenuation.py
@@ -23,24 +23,16 @@ class PyDisortTests(unittest.TestCase):
 
     def test_attenuation(self):
         """Test attenuation with pydisort."""
-        tau = torch.tensor([0.1, 0.2, 0.3, 0.4]).reshape((4, 1))
-        bc = {"fbeam": torch.tensor([3.14159]).reshape((1, 1))}
+        tau = torch.tensor([0.1, 0.2, 0.3, 0.4]).reshape((4,1))
+        bc = {"fbeam" : torch.tensor([3.14159]).reshape((1,1))}
         result = self.ds.forward(tau, bc)
         assert_allclose(
             result,
-            torch.tensor(
-                [
-                    [
-                        [
-                            [0.0000, 3.1416],
-                            [0.0000, 2.8426],
-                            [0.0000, 2.3273],
-                            [0.0000, 1.7241],
-                            [0.0000, 1.1557],
-                        ]
-                    ]
-                ]
-            ),
+            torch.tensor([[[[0.0000, 3.1416],
+                [0.0000, 2.8426],
+                [0.0000, 2.3273],
+                [0.0000, 1.7241],
+                [0.0000, 1.1557]]]]),
             atol=1e-4,
             rtol=1e-4,
         )
